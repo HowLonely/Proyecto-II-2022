abd08898cfbb7a0ab3c0a8bdd7298892c4bbb1d8
maui12 <103455248+maui12@users.noreply.github.com>
2022-06-24 20:34:33 -0400

    barra de vida

--- Assets/Scripts/Entidades/Entidad.cs
+++ Assets/Scripts/Entidades/Entidad.cs
@@ -7,18 +7,23 @@ public class Entidad : MonoBehaviour
     public float currentHealth;
     public float maxHealth;
 
+    //Barra de vida
+    public HealthBar vida;
     public Animator anim;
     public BoxCollider2D bc;
 
 
-    void Start() {
+    void Awake() {
         currentHealth = maxHealth;
+        vida.setMaxHealth(maxHealth);
     }
 
     public void TakeDamage(float damage) {
 
         anim.SetTrigger("hurt");
         currentHealth -= damage;
+
+        vida.setHealth(currentHealth);
 
         if(currentHealth <= 0) {
             Die();
@@ -29,6 +34,10 @@ public class Entidad : MonoBehaviour
         anim.SetBool("die",true);
         bc.size = new Vector2(0.1f,0.1f);
         this.enabled = false;
+    }
+
+    public float getCurrentHealth() {
+        return currentHealth;
     }
 }
 
